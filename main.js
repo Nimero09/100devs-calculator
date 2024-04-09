5c3c17441530c7f38ec5720082af1b8441c8d69f
Nicolas Melo <jaimenicolasmelo@gmail.com>
2024-04-09 16:35:47 -0500

    JS file rename

--- main.js
+++ main.js
@@ -1,97 +1,124 @@
 class Calculator {
-  constructor() {
-    this.screen = '';
-    this.operand = '';
-    this.operation = null;
-    this.firstValue = false;
-    this.a = 0;
-    this.b = 0;
+    constructor() {
+        this.a = 0;
+        this.b = 0;
+        this.screen = '';
+        this.operand = '';
+        this.operation = null;
 
-    this.add = function() {
-      return this.a + this.b;
-    }
+        this.add = function() {
+            return this.a + this.b;
+          }
+      
+          this.substract = function() {
+            return this.a - this.b;
+          }
+      
+          this.multiply = function() {
+            return this.a * this.b;
+          }
+      
+          this.divide = function() {
+            return this.a / this.b;
+        }
 
-    this.substract = function() {
-      return this.a - this.b;
-    }
+        this.setOperation = function(value) {
+            this.operation = value;
+            this.a = Number(this.operand);
+            this.operand = '';
+            this.b = 0;
+        }
 
-    this.multiply = function() {
-      return this.a * this.b;
-    }
+        this.calculate = function() {
+            this.b = Number(this.operand);
+            let result = 0;
+            switch (this.operation) {
+              case 'x':
+                result = this.multiply();
+                break;
+              case '+':
+                result = this.add();
+                  break; 
+              case '-':
+                result = this.substract();
+                break;
+              case '/':
+                result = this.divide()
+                break;
+            }
+            this.operand = result;
+            this.screen = result;
+            this.operation = null;
+          }
 
-    this.divide = function() {
-      return this.a / this.b;
-    }
+        this.updateScreen = function(value) {
+            if (this.b !== 0) {
+                this.screen = value;
+                this.operand = '';
+            } 
+            else if (this.operand[0] === '0') this.screen = value;
+            else this.screen += value;
+        }
 
-    this.setOperation = function(value) {
-      this.operation = value;
-      this.a = this.screen;
-      this.operand = '';
-      this.firstValue = true
-    }
+        this.clear = function() {
+            this.screen = '';
+            this.a = 0;
+            this.operand = '';
+            this.operation = null;
+        }
 
-    this.calculate = function() {
-      this.b = this.operand;
-      let result = 0;
-      switch (this.operation) {
-        case 'x':
-          result = this.multiply();
-          break;
-        case '+':
-          result = this.add();
-            break; 
-        case '-':
-          result = this.substract();
-          break;
-        case '÷':
-          result = this.divide()
-          break;
-      }
-      this.updateScreen(result);
-      this.a = result;
+        this.addOperandNumber = function(value) {
+            if (this.operand[0] === '0') this.operand = value;
+            else this.operand += value;
+        }
     }
-
-    this.updateScreen = function(value) {
-      this.screen += value;
-      this.operand += value;
-    }
-
-    this.clear = function() {
-      this.screen = '';
-      this.firstValue = false;
-      this.operand = '';
-    }
-  }
 }
 
 const calculator = new Calculator();
-
 const screen = document.querySelector('.placeholder');
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const deleteBtn = document.querySelector('[data-delete]');
-const operations = document.querySelectorAll('[data-operation');
-const equalBtn = document.querySelector('[data-equal');
-
+const operations = document.querySelectorAll('[data-operation]');
+const equalBtn = document.querySelector('.data-equal');
+const deleteBtn = document.querySelector('.data-delete');
 
 
 numberButtons.forEach(x => {
-  x.addEventListener('click', () => {
-    calculator.updateScreen(x.innerText);
-    screen.innerText = calculator.screen;
-  })
+    x.addEventListener('click', () => {
+        if (calculator.operand[0] === '0' && x.innerText === '0') {
+            return;
+        }
+        else {
+            calculator.updateScreen(x.innerText);
+            calculator.addOperandNumber(x.innerText);
+            screen.innerText = calculator.screen;
+        }
+      
+    })
 })
 
 operations.forEach(x => {
-  x.addEventListener('click', () => {
-    calculator.setOperation(x.innerText);
-    calculator.updateScreen(x.innerText);
+    x.addEventListener('click', () => {
+    if (calculator.operation) {
+        calculator.calculate();
+    } else {
+        calculator.setOperation(x.innerText);
+        calculator.updateScreen(x.innerText);
+    }
     screen.innerText = calculator.screen;
   })
 })
 
-deleteBtn.addEventListener('click', () => {
-  calculator.clear();
-  screen.innerText = calculator.screen;
+equalBtn.addEventListener('click', () => {
+    if (!calculator.operation) {
+        return;
+    } else {
+        calculator.calculate();
+        screen.innerText = calculator.screen;
+    }
 })
 
+deleteBtn.addEventListener('click', () => {
+    calculator.clear();
+    screen.innerText = calculator.screen;
+})
